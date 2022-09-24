19dffdc2d2063824d7fe9108fa456286f082746a
Ihonosetale Oseghale <onosetaleoseghale@gmail.com>
2022-09-24 10:46:37 +0100

    Updated

--- _exercises.py
+++ _exercises.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from functools import reduce
 import operator
 
@@ -10,6 +9,25 @@ def _sum(iterE, start=0):
     return acc
 
 
-print(reduce(_sum, range(10)))
-=======
->>>>>>> 0021d919c09f35cdcec4ade127846ed0b10d7380
+# print(reduce(_sum, range(10)))
+
+
+def moveNegPos(A):
+
+    for i in range(1, len(A)):
+        key = A[i]
+
+        if key > 0:
+            continue
+
+        j = i - 1
+        while j >= 0 and A[j] > 0:
+            A[j+1] = A[j]
+            j = j - 1
+
+        A[j+1] = key
+
+    return A
+
+
+print(moveNegPos([2, 3, -1, 4, -8]))
